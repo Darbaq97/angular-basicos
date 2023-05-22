81427788296716de969caefe5e6fc65be4350fd4
Darbaq97 <david.k17dma@hotmail.com>
2023-05-21 21:59:06 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Tarea:
+//Crear un nuevo modulo llamado contadorModule
+//Declaraciones, y exportaciones
+import { ListadoComponent } from './heroes/listado/listado.component';
+
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    HeroesModule,
+    ContadorModule,
     AppRoutingModule
   ],
   providers: [],
